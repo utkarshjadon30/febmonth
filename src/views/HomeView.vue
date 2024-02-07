5897b788e535c109b36e2527c1f931fc7e4814fe
utkarshjadon30 <utkarshjadon30@gmail.com>
2024-02-08 00:38:48 +0530

    Hello

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<script setup>
+
+</script>
+
+<template>
+<v-container fluid class="bg-pink-lighten-5 h-screen" >
+  <v-spacer></v-spacer>
+  <v-row>
+    <v-spacer></v-spacer>
+    <v-col col="6">  
+      <v-card >
+        kya aap wahi hai jo mai soch raha hu
+      </v-card>
+    </v-col>
+    <v-spacer></v-spacer>
+  </v-row>
+  <v-spacer></v-spacer>
+
+  
+
+
+  
+
+</v-container>
+</template>
